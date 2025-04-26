bfafccb2b544a96c38873404373363e98796fa85
alkeogaci <keogaci@gmail.com>
2025-04-26 22:36:33 +0200

    spring

--- Backend/Spring/src/main/java/com/projectairbnb/backend/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projectairbnb.backend.repository;
-
-import com.projectairbnb.backend.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-    // You can add custom query methods later if needed
-}
--- Backend/Spring/src/main/java/com/projectairbnb/backend/repository/HostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projectairbnb.backend.repository;
-
-import com.projectairbnb.backend.model.Host;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HostRepository extends JpaRepository<Host, Long> {
-    // You can add custom query methods later if needed
-}
