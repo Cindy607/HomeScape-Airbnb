2ca25d149faaca3c869e92cd585bb40fcaa3fb2c
alkeogaci <keogaci@gmail.com>
2025-04-26 12:08:01 +0200

    Update Apartment dhe Rezervation Classes

--- src/main/java/al/sda/Apartment.java
+++ src/main/java/al/sda/Apartment.java
@@ -1,4 +1,68 @@
 package al.sda;
 
 public class Apartment {
+        private String id;
+        private String name;
+        private String location;
+        private double pricePerNight;
+        private String hostId;
+
+    public Apartment(String id, String name, String location, double pricePerNight, String hostId) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.pricePerNight = pricePerNight;
+        this.hostId = hostId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public String getHostId() {
+        return hostId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void setHostId(String hostId) {
+        this.hostId = hostId;
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", pricePerNight=" + pricePerNight +
+                ", hostId='" + hostId + '\'' +
+                '}';
+    }
 }
--- src/main/java/al/sda/Reservation.java
+++ src/main/java/al/sda/Reservation.java
@@ -1,4 +1,81 @@
 package al.sda;
 
+import java.time.LocalDate;
+
 public class Reservation {
+        private String id;
+        private String clientId;
+        private String propertyId;
+        private LocalDate startDate;
+        private LocalDate endDate;
+        private Boolean status; // true- active ; false-cancelled
+
+    public Reservation(String id, String clientId, String propertyId, LocalDate startDate, LocalDate endDate, Boolean status) {
+        this.id = id;
+        this.clientId = clientId;
+        this.propertyId = propertyId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getPropertyId() {
+        return propertyId;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setPropertyId(String propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id='" + id + '\'' +
+                ", clientId='" + clientId + '\'' +
+                ", propertyId='" + propertyId + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", status=" + status +
+                '}';
+    }
 }
