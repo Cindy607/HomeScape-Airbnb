589660d4d6ed14ff9c977aa618a2adcd16a2e185
Mustavataj13 <klea.mustavataj@cit.edu.al>
2025-04-26 14:19:39 +0200

    database

---
+++ src/main/java/al/sda/Config/HibernateConfiguration.java
@@ -0,0 +1,59 @@
+import al.sda.Entities.Apartment;
+import al.sda.Entities.Host;
+import al.sda.Entities.Reservation;
+import al.sda.Entities.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class HibernateConfiguration {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create Hibernate configuration object
+            Configuration configuration = new Configuration();
+
+            // Load Hibernate properties from hibernate.properties file
+            Properties properties = new Properties();
+            try (InputStream input = HibernateConfiguration.class.getClassLoader()
+                    .getResourceAsStream("hibernate.properties")) {
+                if (input == null) {
+                    throw new RuntimeException("Unable to find hibernate.properties");
+                }
+                properties.load(input);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load hibernate.properties", e);
+            }
+
+            // Set Hibernate properties
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Apartment.class);
+            configuration.addAnnotatedClass(Host.class);
+            configuration.addAnnotatedClass(Reservation.class);
+
+
+            // Build service registry
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+
+            // Create SessionFactory from configuration and service registry
+            return configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
