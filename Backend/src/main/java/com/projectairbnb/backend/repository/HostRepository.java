8869dd80a402e227b54f25ab547087be40ff0d1a
alkeogaci <keogaci@gmail.com>
2025-04-27 09:42:14 +0200

    spring corrected

--- Backend/src/main/java/com/projectairbnb/backend/repository/HostRepository.java
+++ Backend/src/main/java/com/projectairbnb/backend/repository/HostRepository.java
@@ -4,5 +4,5 @@ import com.projectairbnb.backend.model.Host;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HostRepository extends JpaRepository<Host, Long> {
-    // You can add custom query methods later if needed
+
 }
